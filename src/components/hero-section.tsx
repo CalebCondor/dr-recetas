78670888a4acfdf53f691b3608edd52c7be51ec6
CalebCondor <calebcondor553@gmail.com>
2026-01-19 16:43:06 -0500

    característica: Agregar el componente de sección Héroe con una animación de escritura para titulares y una lista animada de tipos de consulta.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -32,12 +32,12 @@ const consultations = [
 
 export default function Hero() {
   return (
-    <main className="relative w-full h-auto min-h-[600px] lg:min-h-[800px] overflow-hidden flex items-center justify-center">
+    <main className="relative w-full h-auto min-h-[600px] lg:min-h-[700px] overflow-hidden flex items-center justify-center">
       {/* Content Layer */}
       <div className="relative z-10 w-full px-6 md:px-12 lg:px-[8%] flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-48 text-center lg:text-left py-12 lg:py-20">
         {/* Left Content */}
         <div className="flex-1 max-w-4xl">
-          <h1 className="text-[2.4rem] leading-tight sm:text-6xl md:text-6xl lg:text-7xl font-bold text-white mb-8 lg:mb-12 tracking-tight font-semibold">
+          <h1 className="text-[2.4rem] font-semibold leading-3 sm:text-6xl md:text-6xl lg:text-7xl text-white mb-8 lg:mb-12 tracking-tight">
             ¿Necesitas una <br className="hidden sm:block" /> {/* desktop */}
             <br className="block sm:hidden" /> {/* mobile */}
             <span className="text-[#6CE4AE]">
